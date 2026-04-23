f4cb91ea3b81beb76ba339d2fafd5a65350a91b3
Gabriel  Buron <106992267+YuzuruTK@users.noreply.github.com>
2026-04-23 10:53:59 -0300

    Revert "Feat/cloudflare pages migration"

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,3 @@
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToReadableStream } from "react-dom/server";
@@ -9,37 +8,31 @@ export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  _loadContext: AppLoadContext
+  remixContext: any
 ) {
-  let shellRendered = false;
   const userAgent = request.headers.get("user-agent");
-  const readyOption = isbot(userAgent ?? "") || remixContext.isSpaMode
-    ? "onAllReady"
-    : "onShellReady";
+  const isBotRequest = isbot(userAgent);
 
   const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} abortDelay={ABORT_DELAY} />,
+    <RemixServer context={remixContext} url={request.url} />,
     {
-      [readyOption]() {
-        shellRendered = true;
-      },
+      signal: AbortSignal.timeout(ABORT_DELAY),
       onError(error: unknown) {
+        console.error(error);
         responseStatusCode = 500;
-        if (shellRendered) {
-          console.error(error);
-        }
       },
     }
   );
 
-  if (isbot(userAgent ?? "")) {
+  if (isBotRequest) {
     await body.allReady;
   }
 
-  responseHeaders.set("Content-Type", "text/html");
   return new Response(body, {
-    headers: responseHeaders,
+    headers: {
+      ...Object.fromEntries(responseHeaders),
+      "Content-Type": "text/html",
+    },
     status: responseStatusCode,
   });
 }
